104c86b9f3990ec7e6e6c2b90793fbfd5631df81
Moorisong <thiagoo@naver.com>
2024-12-31 18:08:56 +0900

    feat: 로그인 페이지 추가

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Main from 'src/routes/Main';
+import Login from 'src/routes/Login';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
