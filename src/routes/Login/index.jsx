d983d732550200671f4860f5bc6e2653368a3b79
Moorisong <thiagoo@naver.com>
2025-01-02 16:10:54 +0900

    feat: 카카오 로그인 api 구현

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,3 +1,17 @@
 export default function Login() {
-  return <div>login</div>;
+  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_APP_KEY}&redirect_uri=${process.env.REACT_APP_LOCAL_URL}/auth&response_type=code`;
+
+  const kakaoLogin = () => {
+    window.location.href = url;
+  };
+
+  return (
+    <div
+      onClick={() => {
+        kakaoLogin();
+      }}
+    >
+      login
+    </div>
+  );
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Main from 'src/routes/Main';
 import Login from 'src/routes/Login';
+import KakaoAuthHandle from 'src/routes/KakaoAuthHandle';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/auth" element={<KakaoAuthHandle />} />
     </Routes>
   );
 }
