72cf96641cda66c290f1df4e49065e2e957543f4
Moorisong <thiagoo@naver.com>
2025-01-04 21:46:24 +0900

    refactor: 미사용 함수 주석 처리

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,6 +1,9 @@
-import axios from 'axios';
 
-export const api_get = async (url) => {
-  const response = await axios.get(url);
-  return response.data;
-};
+// ksh -- 날씨 api 사용을 위한 함수
+
+// import axios from 'axios';
+
+// export const api_get = async (url) => {
+//   const response = await axios.get(url);
+//   return response.data;
+// };
