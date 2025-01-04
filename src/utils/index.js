f61ec070d347a8b69c027249583881fd46dedb35
Moorisong <thiagoo@naver.com>
2025-01-04 21:46:24 +0900

    refactor: 미사용 함수 주석 처리

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,18 +1,3 @@
-import { api_get } from 'src/services/axios';
-import { METHOD_TYPE } from 'src/constants';
-
-export const fetchData = async (url, method) => {
-  let data;
-  switch (method) {
-    case METHOD_TYPE.GET:
-      data = await api_get(url);
-      break;
-    default:
-      return;
-  }
-  return data;
-};
-
 export const kakaoTextShare = (data) => {
   window.Kakao.Share.sendDefault({
     objectType: 'text',
@@ -21,3 +6,20 @@ export const kakaoTextShare = (data) => {
     buttonTitle: `${data.user} 님이 보낸 메시지☺️`,
   });
 };
+
+// ksh -- 날씨 api 사용을 위한 함수
+
+// import { api_get } from 'src/services/axios';
+// import { METHOD_TYPE } from 'src/constants';
+
+// export const fetchData = async (url, method) => {
+//   let data;
+//   switch (method) {
+//     case METHOD_TYPE.GET:
+//       data = await api_get(url);
+//       break;
+//     default:
+//       return;
+//   }
+//   return data;
+// };
