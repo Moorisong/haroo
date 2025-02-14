e9743252ff0d01d21a1b46fcbd48cc75bfb7c708
Moorisong <thiagoo@naver.com>
2025-02-15 03:03:26 +0900

    refactor: 카카오 공유하기 템플릿 변경
    (메시지 -> 피드)

--- src/routes/Main/index.jsx
+++ src/routes/Main/index.jsx
@@ -1,46 +1,40 @@
 import { useState } from 'react';
-import { kakaoTextShare, getYoutubeId, validateYoutubeUrl } from 'src/utils';
+import { getYoutubeId, validateYoutubeUrl, kakaoListShare } from 'src/utils';
 import { TextBoxDefault } from 'src/components/TextBox';
 import { DATA_TYPE } from 'src/constants';
 
 export default function Main() {
   const defalutData = [
-    { title: '보내는 사람', text: '' },
-    { title: '내용', text: '' },
+    { title: '전제', text: '' },
     { title: '유튜브', text: '' },
   ];
   const [data, setData] = useState(defalutData);
 
   const onClickShare = () => {
     if (!window.Kakao.isInitialized()) window.Kakao.init(process.env.REACT_APP_KAKAO_APP_KEY);
-    const youtubeId = getYoutubeId(data[2].text);
+    const youtubeId = getYoutubeId(data[1].text);
     const isValidYoutubeUrl = validateYoutubeUrl(youtubeId);
 
     if (isValidYoutubeUrl) {
-      kakaoTextShare(data);
+      kakaoListShare(data, youtubeId);
       return setData(defalutData);
     }
     return alert('유튜브 동영상 주소를 정확하게 입력해주세요.');
   };
 
   const onChangeText = (event) => {
-    const dataType =
-      event.target.id === '0' ? DATA_TYPE.SENDER : event.target.id === '1' ? DATA_TYPE.CONTENT : DATA_TYPE.YOUTUBE;
+    const dataType = event.target.id === '0' ? DATA_TYPE.CONDITION : DATA_TYPE.YOUTUBE;
 
     setData((prevData) => {
       let newData = [...prevData];
 
       switch (dataType) {
-        case DATA_TYPE.SENDER:
+        case DATA_TYPE.CONDITION:
           newData[0] = { ...newData[0], text: event.target.value };
           break;
 
-        case DATA_TYPE.CONTENT:
-          newData[1] = { ...newData[1], text: event.target.value };
-          break;
-
         case DATA_TYPE.YOUTUBE:
-          newData[2] = { ...newData[2], text: event.target.value };
+          newData[1] = { ...newData[1], text: event.target.value };
           break;
 
         default:
