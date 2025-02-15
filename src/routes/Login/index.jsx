871b2dc83fa406b203e7e69c95246a07b15a75c6
Moorisong <thiagoo@naver.com>
2025-02-15 14:54:55 +0900

    refactor: 로그인 경로 prod url 추가

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,5 +1,6 @@
 export default function Login() {
-  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_APP_KEY}&redirect_uri=${process.env.REACT_APP_LOCAL_URL}/auth&response_type=code`;
+  // const url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_APP_KEY}&redirect_uri=${process.env.REACT_APP_LOCAL_URL}/auth&response_type=code`;
+  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_APP_KEY}&redirect_uri=${process.env.REACT_APP_PROD_URL}/auth&response_type=code`;
 
   const kakaoLogin = () => {
     window.location.href = url;
