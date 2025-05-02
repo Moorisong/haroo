a999fe45777c704d5a0c717db57eb93b3e870ee6
Moorisong <thiagoo@naver.com>
2025-05-02 23:21:33 +0900

    feat: BrandHeader 컴포넌트화

--- src/routes/Login/index.jsx
+++ src/routes/Login/index.jsx
@@ -1,17 +1,14 @@
 import { kakaoLogin } from 'src/utils';
-import { FONT, TITLE_TEXT } from 'src/constants';
+import { FONT } from 'src/constants';
+import BrandHeader from 'src/components/BrandHeader';
 
 export default function Login() {
-  // const url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_APP_KEY}&redirect_uri=${process.env.REACT_APP_LOCAL_URL}/auth&response_type=code`;
-  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_APP_KEY}&redirect_uri=${process.env.REACT_APP_PROD_URL}/auth&response_type=code`;
+  const url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_APP_KEY}&redirect_uri=${process.env.REACT_APP_LOCAL_URL}/auth&response_type=code`;
+  // const url = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_APP_KEY}&redirect_uri=${process.env.REACT_APP_PROD_URL}/auth&response_type=code`;
 
   return (
     <div className="mt-40 flex flex-col items-center gap-8">
-      <img src="/logo.svg" alt="Haroo logo" />
-      <div className="flex flex-col items-center">
-        <span className={FONT.BIG_BLACK}>{TITLE_TEXT.KOREAN}</span>
-        <span className={FONT.MEDIUM_BLUE}>{TITLE_TEXT.ENGLISH}</span>
-      </div>
+      <BrandHeader />
       <span className={`flex gap-1 ${FONT.SMALL_GRAY}`}>
         <span>2025</span>
         <span>·</span>
