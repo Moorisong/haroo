14301dc0d6f3adbe418991cc31d6fb05cad72d2a
Moorisong <thiagoo@naver.com>
2025-05-06 17:42:57 +0900

    feat: luck 페이지 추가, 라우트 업데이트

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,12 +4,14 @@ import Login from 'src/routes/Login';
 import KakaoAuthHandle from 'src/routes/KakaoAuthHandle';
 import Policy from 'src/routes/Policy';
 import Terms from 'src/routes/Terms';
+import Luck from 'src/routes/Luck';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/main" element={<Main />} />
+      <Route path="/luck" element={<Luck />} />
       <Route path="/auth" element={<KakaoAuthHandle />} />
       <Route path="/policy" element={<Policy />} />
       <Route path="/terms" element={<Terms />} />
