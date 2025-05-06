ce1cf60edffeb0ad9d256444c312a96519978c46
Moorisong <thiagoo@naver.com>
2025-05-06 23:22:28 +0900

    fix: 실 도메인 cors 허용

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', 'https://haroo.vercel.app'],
   credentials: true,
 };
 
