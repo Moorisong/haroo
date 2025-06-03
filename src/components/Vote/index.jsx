c2f071d94692e4d783fa8cc56e072380d8a67ae1
Moorisong <thiagoo@naver.com>
2025-06-03 13:05:34 +0900

    refactor: 투표 후 투표 박스 색 채워지도록 개선

--- src/components/Vote/index.jsx
+++ src/components/Vote/index.jsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
+import { DATA_TYPE } from 'src/constants';
 import { TEXT, COMPONENT_STYLE } from 'src/constants';
 import { submitVote } from 'src/services/harooApis';
 
-export default function Vote({ data }) {
+export default function Vote({ voteData, userVoteData }) {
+  const initialVoteIndex = userVoteData.isVoted ? userVoteData.optionIndex : undefined;
+  const [clikedIndex, setClickedIndex] = useState(initialVoteIndex);
   const now = new Date();
   const isVotingClosed = now.getHours() === 23 && now.getMinutes() >= 30;
 
@@ -11,9 +15,10 @@ export default function Vote({ data }) {
     if (!confirmed) return;
 
     try {
-      await submitVote(data._id, index);
+      await submitVote(voteData._id, index);
+      setClickedIndex(index);
     } catch (error) {
-      const message = error.response?.data?.message || error.message || '문제가 발생했어요!';
+      const message = error.response?.data?.message || error.message || DATA_TYPE.ERROR_MESSAGE;
       return alert(message);
     }
   };
@@ -34,12 +39,12 @@ export default function Vote({ data }) {
       <div className={COMPONENT_STYLE.VOTE.WRAPPER}>
         <div className={COMPONENT_STYLE.VOTE.TITLE}>{TEXT.HAROO.VOTE_TITLE}</div>
 
-        <h2 className={COMPONENT_STYLE.VOTE.TOPIC}>{data.topic}</h2>
+        <h2 className={COMPONENT_STYLE.VOTE.TOPIC}>{voteData.topic}</h2>
 
         <div className={COMPONENT_STYLE.VOTE.OPTIONS_GRID}>
-          {data.options.map((e, i) => {
-            const isSelected = i === 0;
-            const isDisabled = isVotingClosed; // ksh 로직 추가하기
+          {voteData.options.map((e, i) => {
+            const isDisabled = isVotingClosed;
+            const isSelected = i === clikedIndex;
 
             return (
               <button
@@ -65,7 +70,7 @@ export default function Vote({ data }) {
         </div>
       </div>
 
-      <div className={COMPONENT_STYLE.VOTE.KNOWLEDGE_BOX}>{formatWithLineBreaks(data.knowledge)}</div>
+      <div className={COMPONENT_STYLE.VOTE.KNOWLEDGE_BOX}>{formatWithLineBreaks(voteData.knowledge)}</div>
     </>
   );
 }
